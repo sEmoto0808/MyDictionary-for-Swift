1ac08d0673d0799eae65b83d307a17fa5eb5a3e3
s.emoto <sho.emoto@gmail.com>
2017-07-10 03:56:34 +0900

    TableViewをセルごとに開閉する例

---
+++ TableViewController.swift
@@ -0,0 +1,134 @@
+//
+//  TableViewController.swift
+//  new
+//
+//  Created by 江本匠 on 2017/07/10.
+//  Copyright © 2017年 st-ventures.jp. All rights reserved.
+//
+
+import UIKit
+
+class TableViewController: UITableViewController {
+    
+    private var openedSections = Set<Int>()
+    private let items = [
+        (type: "キュート", members: ["双葉", "緒方", "三村", "前川", "島村"]),
+        (type: "クール", members: ["神崎", "多田", "アナ", "新田", "渋谷"]),
+        (type: "パッション", members: ["諸星", "城ヶ崎", "赤城", "本田"]),
+        (type: "ミシロ", members: ["美城"]),
+        ]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    func tapSectionHeader(sender: UIGestureRecognizer) {
+        if let section = sender.view?.tag {
+            if self.openedSections.contains(section) {
+                self.openedSections.remove(section)
+            } else {
+                self.openedSections.insert(section)
+            }
+            
+            self.tableView.reloadSections(IndexSet(integer: section), with: .fade)
+        }
+    }
+    
+    
+    
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return self.items.count
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if self.openedSections.contains(section) {
+            return self.items[section].members.count
+        } else {
+            return 0
+        }
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = self.items[indexPath.section].members[indexPath.row]
+        
+        return cell
+    }
+    
+    // MARK: Table view delegate
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return self.items[section].type
+    }
+    
+    override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let view = UITableViewHeaderFooterView()
+        let gesture = UITapGestureRecognizer(target: self, action: #selector(self.tapSectionHeader(sender:)))
+        view.addGestureRecognizer(gesture)
+        view.tag = section
+        
+        return view
+    }
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
