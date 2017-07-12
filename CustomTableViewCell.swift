4507da592147fab775e16c8928f52ab7b93491b7
s.emoto <sho.emoto@gmail.com>
2017-07-13 01:33:55 +0900

    TableViewのセルを設定するクラスを作成 今後随時更新

---
+++ CustomTableViewCell.swift
@@ -0,0 +1,33 @@
+//
+//  CustomTableViewCell.swift
+//  new
+//
+//  Created by 江本匠 on 2017/07/13.
+//  Copyright © 2017年 st-ventures.jp. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    func setImage (url: String) {
+        
+        let url = NSURL(string: url)
+        let imageData = try? Data(contentsOf: url! as URL)
+        let image = UIImage(data:imageData!)
+        self.imageView?.image = image
+        
+    }
+
+}
