8f482515301e2818df22fe8ed0eb89dd7c1fc8ad
江本匠 <sho.emoto@gmail.com>
2017-07-23 08:50:45 +0900

    実装の改善版コードと実装時の考え方を追加

--- TextFieldがキーボードで隠れないようにする/CustomKeyboardViewController.swift
+++ TextFieldがキーボードで隠れないようにする/CustomKeyboardViewController.swift
@@ -7,22 +7,45 @@
 //
 
 import UIKit
+import RealmSwift
 
 class CustomKeyboardViewController: UIViewController {
+    
+    let config = Realm.Configuration(
+        schemaVersion: 1,
+        migrationBlock: { migration, oldSchemaVersion in
+            if (oldSchemaVersion < 1) {}
+    })
+    
+    var realm: Realm?
 
     @IBOutlet weak var lineTableView: UITableView!
     @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var textField: UITextField!
     @IBOutlet weak var coverView: UIView!
+    @IBOutlet weak var textView: UITextView!
+    @IBOutlet weak var messageBarBottom: NSLayoutConstraint!
+    
+    var items = [String]()
+    var messageResult: Results<Object>?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        textField.delegate = self
+        // セルの線を消す
+        lineTableView.tableFooterView = UIView(frame: .zero)
         
         //タップでキーボードを閉じるジェスチャー登録
+        
         let scViewTapGesture:UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(hideKeyBoard(_:)))
-        coverView.addGestureRecognizer(scViewTapGesture)
+        self.view.addGestureRecognizer(scViewTapGesture)
+        
+        realm = try! Realm()
+        
+        guard let messageRealm = realm else {
+            return
+        }
+        setItemsFromRealm(realm: messageRealm)
 
     }
     
@@ -46,10 +69,6 @@ class CustomKeyboardViewController: UIViewController {
         // キーボードを表示した直後
         notificationCenter.addObserver(self, selector: #selector(CustomKeyboardViewController.handleKeyboardDidShowNotification(_:)), name: NSNotification.Name.UIKeyboardDidShow, object: nil)
     }
-    
-    
-    
-    
 
 }
 
@@ -60,6 +79,67 @@ extension CustomKeyboardViewController: UITextFieldDelegate {
         // キーボードを閉じる
         textField.resignFirstResponder()
         return true
+    }
+    
+}
+
+// MARK: UITableViewDataSource
+extension CustomKeyboardViewController: UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return items.count
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return 1
+        
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "LINECell", for: indexPath as IndexPath) as! CustomKeyboardTableViewCell
+        
+        cell.setUserMessage(userMessageString: items[indexPath.section])
+        //cell.layer.cornerRadius = 30.0
+        //cell.layer.masksToBounds = true
+        
+        return cell
+    }
+}
+
+extension CustomKeyboardViewController: UITableViewDelegate {
+    
+    /*
+     func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+     
+     // セクションに表示するテキストを返す
+     return "クーポン名"
+     }
+     */
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        
+        // セルの上部のスペース
+        return 20
+    }
+    
+    func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+        
+        // セルの下部のスペース
+        return 20
+    }
+    
+    func tableView(_ tableView: UITableView, willDisplayHeaderView view: UIView, forSection section: Int) {
+        
+        // 透明にすることでスペースとする
+        view.tintColor = UIColor.clear
+    }
+    
+    func tableView(_ tableView: UITableView, willDisplayFooterView view: UIView, forSection section: Int) {
+        
+        // 透明にすることでスペースとする
+        view.tintColor = UIColor.clear
     }
     
 }
@@ -76,7 +156,7 @@ extension CustomKeyboardViewController {
         // キーボードの高さを取得し、テキストフィールドをキーボードの高さ分上に移動する
         let userInfo = (notification as NSNotification).userInfo!
         let keyboardScreenEndFrame = (userInfo[UIKeyboardFrameEndUserInfoKey] as! NSValue).cgRectValue
-        scrollView.contentOffset.y = keyboardScreenEndFrame.size.height
+        messageBarBottom.constant = keyboardScreenEndFrame.size.height
         
     }
     
@@ -84,7 +164,7 @@ extension CustomKeyboardViewController {
     func handleKeyboardWillHideNotification(_ notification: Notification) {
         
         // テキストフィールドを画面の底に移動
-        scrollView.contentOffset.y = 0
+        messageBarBottom.constant = 0
         
     }
     
@@ -93,14 +173,53 @@ extension CustomKeyboardViewController {
     }
     
     func hideKeyBoard(_ sender: UITapGestureRecognizer) {
-        if textField.isFirstResponder {
+        if textView.isFirstResponder {
             // キーボードを消す
-            textField.resignFirstResponder()
+            textView.resignFirstResponder()
+        }
+    }
+    
+    func saveRealm(realm: Realm, message: String) {
+        let messageFromUser = UserMessage(value: ["message": message])
+        let uniqueObj = UniqueObject(value: ["messageId": 0])
+        
+        OperateRealmSwift().saveToRealmSwift(realm: realm, object: messageFromUser)
+        OperateRealmSwift().saveUniqueObjectToRealmSwift(realm: realm, uniqueObject: uniqueObj)
+    }
+    
+    func setItemsFromRealm(realm: Realm) {
+        messageResult = OperateRealmSwift().selectRealmSwift(realm: realm, object: UserMessage.self)
+        
+        var int = 0
+        guard let result = messageResult else {
+            return
+        }
+        int = result.count
+        for i in 0..<int {
+            items.append(result[i]["message"] as! String)
         }
     }
     
     // IBAction
     @IBAction func didTapTransmitMessage(_ sender: Any) {
+        
+        items.removeAll()
+        
+        guard let userText = textView.text else {
+            return
+        }
+        
+        guard let messageRealm = realm else {
+            return
+        }
+        
+        saveRealm(realm: messageRealm, message: userText)
+        
+        setItemsFromRealm(realm: messageRealm)
+        
+        textView.resignFirstResponder()
+        
+        lineTableView.reloadData()
     }
     
 }
